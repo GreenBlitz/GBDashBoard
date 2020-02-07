bb6c2ce6f62bad95d7f64584d908444fc5c7f668
Alexey <theemailreader300@gmail.com>
2020-02-07 19:39:13 +0200

    No longer eating 20 GB of RAM

--- gbdashboard/server_master.py
+++ gbdashboard/server_master.py
@@ -1,6 +1,7 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, Response
 
 from gbdashboard.constants.net import LOCAL_SERVER_IP, SERVER_PORT
+from gbdashboard.dashboard.dashboard_webpage_builder import build_dashboards
 from gbdashboard.tools.pi import route_pi
 
 app = Flask(__name__)
@@ -16,7 +17,15 @@ def send_js(path):
     return send_from_directory('scripts', path)
 
 
-route_pi(app)
+@app.after_request
+def prevent_cache(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate, public, max-age=0"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
 
 if __name__ == '__main__':
+    route_pi(app)
+    build_dashboards(app)
     app.run(host=LOCAL_SERVER_IP, port=SERVER_PORT)
